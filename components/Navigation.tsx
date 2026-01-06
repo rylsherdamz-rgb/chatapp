2092168d32cf75c5d99fe950dc0197a0a5972d6a
rylsherdamz-rgb <rylsherdamz@gmail.com>
2026-01-06 20:12:51 +0800

    updated the ui for the navigation bar

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -3,10 +3,10 @@
 
 
 export default function Navigation() {
-    return <div className="w-full h-[10vh] bg-black px-10 flex justify-between flex-row">
-        <div className="flex flex-row gap-x-5  items-center">
-        <div className="bg-blue-200 w-13 h-13 rounded-full">
-        <p className="text-white text-lg mx-17 my-3 ">
+    return <div className="w-full h-[10vh] bg-black lg:px-10 flex justify-between flex-row">
+        <div className="flex flex-row gap-x-1 px-3  items-center">
+        <div className="bg-blue-200 lg:w-13 lg:h-13 flex flex-row items-center w-8 h-8 rounded-full">
+        <p className="text-white text-lg lg:mx-17 mx-10 lg:my-3 ">
             Chatify
         </p>
         </div>
